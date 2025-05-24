09f35e298e73cb5f047787194942f378a799708f
Steven Degutis <sbdegutis@gmail.com>
2025-05-24 15:20:15 -0500

    Does this actually work?

--- src/core/listener.ts
+++ src/core/listener.ts
@@ -1,22 +1,2 @@
-export type ListenerDone = () => void
+export * from 'ref.api.90s.dev/listener.js'
 
-export class Listener<T = void, U = void> {
-
-  list = new Set<(data: T) => U>()
-
-  dispatch(data: T) {
-    for (const fn of this.list) {
-      fn(data)
-    }
-  }
-
-  watch(fn: (data: T) => U): ListenerDone {
-    this.list.add(fn)
-    return () => { this.list.delete(fn) }
-  }
-
-  clear() {
-    this.list.clear()
-  }
-
-}
