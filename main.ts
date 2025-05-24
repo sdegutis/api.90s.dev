b41ca4b9875544da6e098ae15dfdf8d1791ee714
Steven Degutis <sbdegutis@gmail.com>
2025-05-24 14:39:43 -0500

    Updating build file.

--- main.ts
+++ main.ts
@@ -1,18 +1,65 @@
-import { publishDir } from 'quickexport'
+import { transformSync } from '@babel/core'
+import { DevServer, FileTree, generateFiles, Pipeline } from "immaculata"
+import { transformImportsPlugin } from 'immaculata/babel.js'
+import { readFileSync, rmSync } from 'node:fs'
+import { join } from 'node:path'
 
-publishDir({
 
-  projectRoot: import.meta.dirname,
 
-  srcDir: 'src',
+const isDev = process.argv[2] === 'dev'
 
-  dev: process.argv[2] === 'dev' && {
-    port: 8181,
-    generateFiles: true,
-  },
 
-  importMap: {
-    'react/jsx-runtime': '/_jsx.js'
-  }
 
-})
+const src = new FileTree('src', import.meta.dirname)
+
+const pkgjson = JSON.parse(readFileSync(join(import.meta.dirname, 'package.json'), 'utf8'))
+const prefix = new URL(pkgjson.homepage).pathname.replace(/\/+$/, '')
+
+if (isDev) {
+  const server = new DevServer(8181, { prefix })
+  processSite(server)
+  src.watch().on('filesUpdated', () => {
+    processSite(server)
+  })
+}
+else {
+  processSite()
+}
+
+function processSite(server?: DevServer) {
+  const files = Pipeline.from(src.files)
+
+  files.with(/\.tsx?$/).without(/\.d\.ts$/).do(file => {
+    const result = transform(file.path, file.text)!
+    file.path = file.path.replace(/\.tsx?$/, '.js')
+
+    const mapPath = file.path + '.map'
+    const sourceMapPart = '\n//# sourceMappingURL=' + prefix + mapPath
+    file.text = result.code! + sourceMapPart
+
+    files.add(mapPath, JSON.stringify(result.map))
+  })
+
+  const map = files.results()
+
+  if (server) server.files = map
+
+  rmSync('docs', { force: true, recursive: true })
+  generateFiles(map)
+
+  return map
+}
+
+function transform(path: string, text: string) {
+  return transformSync(text, {
+    sourceMaps: true,
+    filename: path,
+    plugins: [
+      ['@babel/plugin-transform-typescript', { isTSX: true }],
+      ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
+      transformImportsPlugin(import.meta.dirname, {
+        'react/jsx-runtime': prefix + '/core/jsx.js'
+      }),
+    ],
+  })
+}
